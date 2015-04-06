834b5dcdca592b2e938bcf0ecb7722b383ff0fa5
Noah Goldsmid <noahgoldsmid@live.nl>
2015-04-06 17:29:25 +0200

    Node iets verder uitgewerkt

--- src/network/Node.java
+++ src/network/Node.java
@@ -1,13 +1,49 @@
 package network;
 
+import network.connection.*;
+
+
 /**
  * Created by cotix on 4/2/15.
  */
-public class Node {
-    private int ip;
+public class Node extends Thread {
+    private String ip;
+    private short port;
+    private Connection con;
 
-    public Node(int IPaddress) {
+    public Node(String IPaddress, short port) {
         ip = IPaddress;
+        con = null;
+        this.port = port;
+    }
+
+    public boolean isConnected() {
+        if (con == null) {
+            return false;
+        }
+        return con.isConnected();
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void connect() {
+        connect(new TCPConnection());
+    }
+
+    public void connect(Connection c) {
+        if (isConnected() == false) {
+            con = c;
+            c.connect(ip, port);
+        }
+    }
+
+    public void handleConnection() {
+        if (!isConnected()) {
+            return;
+        }
+        con.handleConnection();
     }
 
 }
