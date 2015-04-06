8b4af1bf9b2b586bb3d61dbb3d266223a760ecfd
Noah Goldsmid <noahgoldsmid@live.nl>
2015-04-06 17:29:09 +0200

    Connection interface verbeterd

--- src/network/connection/Connection.java
+++ src/network/connection/Connection.java
@@ -1,10 +1,13 @@
 package network.connection;
 
+import network.connection.packet.Packet;
+
 /**
  * Created by cotix on 4/2/15.
  */
 public interface Connection {
     public void connect();
+    public void connect(String ipAddress, short port);
     public void disconnect();
     public void sendPacket(Packet pckt);
     public void handleConnection();
