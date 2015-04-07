20a435761220af8e3a2fb7b59b0351daf8baf046
destion <m.sneekes@student.utwente.nl>
2015-04-07 12:44:21 +0200

    Initial commit for client and some view files, nothing important

---
+++ src/client/view/view/ChatFrame.java
@@ -0,0 +1,23 @@
+package client.view.view;
+
+import javax.swing.*;
+
+/**
+ * Created by destion on 7-4-15.
+ */
+public class ChatFrame extends JFrame {
+
+    public ChatFrame(){
+        super("ClientName PH");
+        this.setSize(400, 400);
+        this.setVisible(true);
+        this.setResizable(false);
+    }
+
+
+
+
+    public void send(String message){
+
+    }
+}
