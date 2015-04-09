9efaafbb7f99232142ccee80fbf1d7c6a6f6cde1
Noah Goldsmid <noahgoldsmid@live.nl>
2015-04-09 22:49:38 +0200

    Updated the Crypto unit test

--- src/testing/unittests/CryptoKeyPairTest.java
+++ src/testing/unittests/CryptoKeyPairTest.java
@@ -1,6 +1,7 @@
 package testing.unittests;
 
 import client.security.CryptoKeyPair;
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
@@ -30,6 +31,19 @@ public class CryptoKeyPairTest implements UnitTest {
         }
         if (keyAPublic.verify(data, keyB.sign(data))) {
             return "Verifying returns true even though its a different key!";
+        }
+        CryptoKeyPair copyOfA  = new CryptoKeyPair(keyA.getRawPublicKey());
+        CryptoKeyPair copyOfA2 = copyOfA;
+        try {
+            copyOfA2 = new CryptoKeyPair(keyA.toString());
+        } catch (Base64DecodingException e) {
+            e.printStackTrace();
+        }
+        if (!Arrays.equals(keyA.decrypt(copyOfA.encrypt(data)), data)) {
+            return "Constructing a public key using raw public key not working";
+        }
+        if (!Arrays.equals(keyA.decrypt(copyOfA2.encrypt(data)), data)) {
+            return "Constructing a public key using base64 public key not working";
         }
 
         return null;
