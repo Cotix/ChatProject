2d0897df7048b61ab26b01d3d165af9e1d5dccf4
Noah Goldsmid <noahgoldsmid@live.nl>
2015-04-09 22:57:16 +0200

    Updated Address to use a CryptoKeyPair

--- src/network/Address.java
+++ src/network/Address.java
@@ -2,19 +2,26 @@ package network;
 
 //A simple class that allows us for advanced address handling
 
+import client.security.CryptoKeyPair;
+import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
+
 public class Address {
-    private String address;
+    private CryptoKeyPair address;
     private String nickName;
 
     public Address(String address) {
-        this.address = address;
+        try {
+            this.address = new CryptoKeyPair(address);
+        } catch (Base64DecodingException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean equals(Object o) {
-        return o instanceof Address && address.equals(o.toString());
+        return o instanceof Address && toString().equals(o.toString());
     }
 
     public String toString() {
-        return address;
+        return address.toString();
     }
 }
