dec94131180d12f6556efba6b9e6ea320fb65e0b
Erik Gaal <git@erikgaal.nl>
2015-04-10 14:04:26 +0200

    JavaDoc

--- src/client/controller/ClientController.java
+++ src/client/controller/ClientController.java
@@ -5,13 +5,11 @@ import client.security.CryptoKeyPair;
 import network.Address;
 import settings.Configuration;
 
-import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.Map;
 
-
 /**
- * Created by martijn on 8-4-15.
+ * ClientController which is the central controller of the client.
  */
 public class ClientController {
 
@@ -19,26 +17,47 @@ public class ClientController {
     private NetworkController networkController;
     private Map<String, Chat> chats;
 
+    /**
+     * Constructs a ClientController with a new keypair, map of chats and a NetworkController.
+     */
     public ClientController() {
         myKeyPair = new CryptoKeyPair();
-        chats  = new HashMap<>();
-        this.networkController =  new NetworkController(Configuration.HOST, Configuration.PORT, myKeyPair);
+        chats = new HashMap<>();
+        this.networkController = new NetworkController(Configuration.HOST, Configuration.PORT, myKeyPair);
     }
 
+    /**
+     * Constructs a ClientController with parameters for the NetworkController.
+     * @param host node host
+     * @param port node port
+     */
     public ClientController(String host, short port) {
         this.myKeyPair = new CryptoKeyPair();
         this.networkController = new NetworkController(host, port, this.myKeyPair);
     }
 
+    /**
+     * Returns the NetworkController
+     * @return
+     */
     public NetworkController getNetworkController() {
         return networkController;
     }
 
+    /**
+     * TODO: Obsolete?
+     * Create as a Chat object and adds it to the list.
+     * @param address address of the receiver
+     */
     public void addChat(Address address) {
         Chat chat = new Chat(address);
         chats.put(address.getNickName(), chat);
     }
 
+    /**
+     * Returns all chats
+     * @return
+     */
     public Map<String, Chat> getChats() {
         return chats;
     }
