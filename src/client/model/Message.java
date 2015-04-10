e589fb95d50dddc2aa1591437ac8f04cc0fdec71
Noah Goldsmid <noahgoldsmid@live.nl>
2015-04-10 02:02:40 +0200

    makePacket() ipv toString in Message

--- src/client/model/Message.java
+++ src/client/model/Message.java
@@ -1,5 +1,13 @@
 package client.model;
 
+import client.security.CryptoKeyPair;
+import network.connection.packet.Packet;
+import network.connection.packet.PacketUtils;
+import network.connection.packet.StringPacket;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
 /**
  * Created by destion on 7-4-15.
  */
@@ -7,34 +15,58 @@ public class Message {
 
     private long timestamp;
     private String message;
-    private String publicKeySender;
-    private String publicKeyReceiver;
+    private CryptoKeyPair keySender;
+    private CryptoKeyPair keyReceiver;
 
 
     //TODO public key
 
-    public Message(String message, String sender, String receiver, long timestamp){
+    public Message(String message, CryptoKeyPair sender, CryptoKeyPair receiver, long timestamp){
         this.timestamp = timestamp;
         this.message = message;
-        this.publicKeySender = sender;
-        this.publicKeyReceiver = receiver;
+        this.keySender = sender;
+        this.keyReceiver = receiver;
     }
 
     public String getMessage(){
         return this.message;
     }
     public String getPublicKeySender(){
-        return this.publicKeySender;
+        return this.keySender.toString();
     }
     public String getPublicKeyReceiver(){
-        return this.publicKeyReceiver;
+        return this.keyReceiver.toString();
     }
     public long getTimestamp(){
         return this.timestamp;
     }
 
+    public Packet makePacket() {
+        byte[] msg;
+        try {
+            msg = ByteBuffer.allocate(8 + message.length())
+                    .putLong(timestamp)
+                    .put(message.getBytes("US-ASCII"))
+                    .array();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+        byte[] sign = keySender.sign(msg);
+        byte[] encryptedBlob = new byte[msg.length + sign.length];
+        System.arraycopy(sign, 0, encryptedBlob, 0, sign.length);
+        System.arraycopy(msg, 0, encryptedBlob, sign.length, msg.length);
+        encryptedBlob = keyReceiver.encrypt(encryptedBlob);
+        byte[] recvKey = keyReceiver.getRawPublicKey();
+        byte[] data = new byte[recvKey.length + encryptedBlob.length];
+        // [Destination key] [encryptedmessage]
+        System.arraycopy(recvKey, 0, data, 0, recvKey.length);
+        System.arraycopy(encryptedBlob, 0, data, recvKey.length, encryptedBlob.length);
+        return new StringPacket(data, PacketUtils.PacketType.MESSAGE);
+    }
+
     public String toString(){
         //TODO format so node understands
-        return String.format("%s %s %s %s \n", timestamp, publicKeySender, publicKeyReceiver, message);
+        return String.format("%s %s %s %s \n", timestamp, getPublicKeySender(), getPublicKeyReceiver(), message);
     }
 }
