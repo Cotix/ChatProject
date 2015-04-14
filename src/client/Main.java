a3526287d5833f572c9b30b762c9c17a8f2b514d
destion <m.sneekes@student.utwente.nl>
2015-04-14 10:56:25 +0200

    updated TODO lijst

--- src/client/Main.java
+++ src/client/Main.java
@@ -2,12 +2,14 @@ package client;
 
 import client.controller.ClientController;
 import log.Log;
+import log.LogLevel;
 import node.LocalNode;
 
 public class Main {
     public static void main(String[] args) {
         com.sun.org.apache.xml.internal.security.Init.init();
         Log.enableAllLevels();
+        Log.disableLevel(LogLevel.NONE);
 
         LocalNode node = new LocalNode("192.168.5.4");
         node.start();
